22f19df79beb3d46fd3ec7fa7ec88e7ae6c47dd0
Anmol Sangha <anmolsangha02@gmail.com>
2024-10-17 22:39:27 +0530

    Set up main application routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import Login from './pages/login';
+import UsersList from './pages/UserList';
+import EditUser from './components/EditUser';
+import NotFound from './pages/NotFound';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const fetchUsers = async () => {
+    try {
+      const response1 = await axios.get(`https://reqres.in/api/users?page=1`);
+      const response2 = await axios.get(`https://reqres.in/api/users?page=2`);
+      const combinedUsers = [...response1.data.data, ...response2.data.data];
+
+      setUsers(combinedUsers);
+      setTotalPages(2);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const updateUserInList = (updatedUser) => {
+    setUsers((prevUsers) =>
+      prevUsers.map((user) => (user.id === updatedUser.id ? updatedUser : user))
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/users"
+          element={<UsersList users={users} page={page} setPage={setPage} totalPages={totalPages} />}
+        />
+        <Route
+          path="/edit-user/:id"
+          element={<EditUser users={users} updateUserInList={updateUserInList} />}
+        />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
